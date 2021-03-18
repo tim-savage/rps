c1bd3531ec000b2303ae0957de0b8f06110e2a1d
Tim Savage <tim.savage@gmail.com>
2021-03-18 00:39:07 -0500

    reverse wins/losses in table

--- Sources/rps/ScoreTable.swift
+++ Sources/rps/ScoreTable.swift
@@ -10,17 +10,17 @@ class ScoreTable {
 	static let SCORE_TABLE = [
 		Hand.rock: [
 			Hand.rock: Result.draw,
-			Hand.paper: Result.win,
-			Hand.scissors: Result.loss
-		],
-		Hand.paper: [
-			Hand.rock: Result.loss,
-			Hand.paper: Result.draw,
+			Hand.paper: Result.loss,
 			Hand.scissors: Result.win
 		],
-		Hand.scissors: [
+		Hand.paper: [
 			Hand.rock: Result.win,
-			Hand.paper: Result.loss,
+			Hand.paper: Result.draw,
+			Hand.scissors: Result.loss
+		],
+		Hand.scissors: [
+			Hand.rock: Result.loss,
+			Hand.paper: Result.win,
 			Hand.scissors: Result.draw
 		]
 	]
