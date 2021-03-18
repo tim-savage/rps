830a05f20b6ce27f301b77ef67f62410a4cebc46
Tim Savage <tim.savage@gmail.com>
2021-03-18 10:19:25 -0500

    rollback last change

--- Sources/rps/ScoreBoard.swift
+++ Sources/rps/ScoreBoard.swift
@@ -70,7 +70,7 @@ class ScoreBoard {
 		numberFormatter.maximumFractionDigits = 4
 
 
-		// calculate max character width of formatted throw count values for all players
+		// calculate max character width of formatted throw counts values for all players
 		var maxWidth: Int = 0
 		for player in players {
 			for hand in Hand.allCases {
@@ -82,21 +82,21 @@ class ScoreBoard {
 			let winPercentage: Float = Float(player.wins) / Float(rounds)
 			print("\(player.name) won",
 				  numberFormatter.string(from: NSNumber(value: player.wins))!, "times",
-				  "(" + (percentFormatter.string(from: NSNumber(value: winPercentage)) ?? "0") + ")",
-				  "in", (numberFormatter.string(from: NSNumber(value: player.totalThrows)) ?? "0"), "throws.")
+				  "(" + percentFormatter.string(from: NSNumber(value: winPercentage))! + ")",
+				  "in", numberFormatter.string(from: NSNumber(value: player.totalThrows))!, "throws.")
 
 			for hand in Hand.allCases {
 				let throwCountPercentage: Float = Float(player.throwCounts[hand] ?? 0) / Float(player.totalThrows)
 				print("\t\(hand):".padding(toLength: 10, withPad: " ", startingAt: 0),
-					  numberFormatter.string(from: NSNumber(value: player.throwCounts[hand] ?? 0)) ?? "0"
+					  numberFormatter.string(from: NSNumber(value: player.throwCounts[hand] ?? 0))!
 						.leftPadding(toLength: maxWidth, withPad: " "),
-					  "(" + (percentFormatter.string(from: NSNumber(value: throwCountPercentage)) ?? "0") + ")")
+					  "(" + percentFormatter.string(from: NSNumber(value: throwCountPercentage))! + ")")
 			}
 			print()
 		}
 
 		let throwsPerRound: Float = Float(cumulativeThrows) / Float(rounds)
-		print("Average throws per round:", numberFormatter.string(from: NSNumber(value: throwsPerRound)) ?? "0")
+		print("Average throws per round:", numberFormatter.string(from: NSNumber(value: throwsPerRound))!)
 		print("Maximum throws in a round:", numberFormatter.string(from: NSNumber(value: mostThrows))!)
 
 		if draws > 0 {
