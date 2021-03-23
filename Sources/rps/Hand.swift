892987df84326eb9c292475a9f63a7d0de13d137
Tim Savage <tim.savage@gmail.com>
2021-03-23 07:41:47 -0500

    refactor getRandom method

--- Sources/rps/Hand.swift
+++ Sources/rps/Hand.swift
@@ -5,21 +5,36 @@
 //  Created by Tim Savage on 3/16/21.
 //
 
-enum Hand: CaseIterable {
+enum Hand: Int, CaseIterable {
 	case rock
 	case paper
 	case scissors
 	
 	static func getRandom(weights: [Int]) -> Hand {
-		
-		let r = Int.random(in: 1...weights.sum())
-		if r <= weights[0] {
-			return .rock
+
+		// if array contains at least one element, compute weights
+		if (weights.count > 0) {
+
+			var mutableWeights = weights
+			
+			// populate array with zeros to fill to length of this enum
+			while (mutableWeights.count < self.allCases.count) {
+				mutableWeights.append(0)
+			}
+
+			// if array is not all zeros, choose weighted element
+			if !mutableWeights.elementsEqual(repeatElement(0, count: self.allCases.count)) {
+				let r = Int.random(in: 1...mutableWeights.sum())
+				for element in self.allCases {
+					if r <= mutableWeights[0...element.rawValue].sum() {
+						return element
+					}
+				}
+			}
 		}
-		else if r <= weights[0] + weights[1] {
-			return .paper
-		}
-		return .scissors
+
+		// fallback returns unweighted random element
+		return self.allCases.randomElement()!
 	}
 }
 
