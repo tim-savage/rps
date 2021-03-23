7b1f3e90874e975a522de4dbfe3d59ba8d731e00
Tim Savage <tim.savage@gmail.com>
2021-03-23 07:42:37 -0500

    refactor to use get ratio methods

--- Sources/rps/ScoreBoard.swift
+++ Sources/rps/ScoreBoard.swift
@@ -58,65 +58,67 @@ class ScoreBoard {
 
 		printResultHeader()
 
+		let numberFormatter = NumberFormatter()
+		numberFormatter.numberStyle = .decimal
+		numberFormatter.minimumFractionDigits = 0
+		numberFormatter.maximumFractionDigits = 4
+
 		let percentFormatter = NumberFormatter()
 		percentFormatter.numberStyle = .percent
 		percentFormatter.multiplier = 100
 		percentFormatter.minimumFractionDigits = 2
 		percentFormatter.maximumFractionDigits = 2
 
-		let numberFormatter = NumberFormatter()
-		numberFormatter.numberStyle = .decimal
-		numberFormatter.minimumFractionDigits = 0
-		numberFormatter.maximumFractionDigits = 4
 
-
-		// calculate max character width of formatted throw counts values for all players
-		var maxCountWidth: Int = 0
-		var maxPercentWidth: Int = 0
+		// calculate max character width of formatted throw count values for all players
+		var widthCol1 = 0
+		var widthCol2 = 0
+		var widthCol3 = 0
 		for player in players {
 			for hand in Hand.allCases {
-				maxCountWidth = max(maxCountWidth, numberFormatter
-										.string(from: NSNumber(value: player.throwCounts[hand] ?? 0))?.count ?? 0)
-				let throwCountPercentage: Float = Float(player.throwCounts[hand] ?? 0) / Float(player.totalThrows)
-				maxPercentWidth = max(maxPercentWidth, percentFormatter
-										.string(from: NSNumber(value: throwCountPercentage))?.count ?? 0)
+				widthCol1 = max(widthCol1, String(describing: hand).count)
+				widthCol2 = max(widthCol2, numberFormatter.string(from: NSNumber(value: player.throwCounts[hand] ?? 0))?.count ?? 0)
+				widthCol3 = max(widthCol3, percentFormatter.string(from: NSNumber(value: player.getThrowRatio(hand)))?.count ?? 0)
 			}
 		}
 
 		for player in players {
-			let winPercentage: Float = Float(player.wins) / Float(rounds)
 			print("\(player.name) won",
 				  numberFormatter.string(from: NSNumber(value: player.wins))!, "times",
-				  "(" + percentFormatter.string(from: NSNumber(value: winPercentage))! + ")",
+				  "(" + percentFormatter.string(from: NSNumber(value: player.getWinRatio(rounds)))! + ")",
 				  "in", numberFormatter.string(from: NSNumber(value: player.totalThrows))!, "throws.")
 
 			for hand in Hand.allCases {
-				let throwCountPercentage: Float = Float(player.throwCounts[hand] ?? 0) / Float(player.totalThrows)
-				print("\t\(hand):".padding(toLength: 10, withPad: " ", startingAt: 0),
+				print("\t\(hand):".padding(toLength: widthCol1 + 2, withPad: " ", startingAt: 0),
 					  numberFormatter.string(from: NSNumber(value: player.throwCounts[hand] ?? 0))!
-						.leftPadding(toLength: maxCountWidth, withPad: " "),
-					  ("(" + percentFormatter.string(from: NSNumber(value: throwCountPercentage))! + ")")
-						.leftPadding(toLength: maxPercentWidth+2, withPad: " "))
+						.leftPadding(toLength: widthCol2, withPad: " "),
+					  ("(" + percentFormatter.string(from: NSNumber(value: player.getThrowRatio(hand)))! + ")")
+						.leftPadding(toLength: widthCol3 + 2, withPad: " "))
 			}
 			print()
 		}
 
-		let throwsPerRound: Float = Float(cumulativeThrows) / Float(rounds)
+		let throwsPerRound = Float(cumulativeThrows) / Float(rounds)
 		print("Average throws per round:", numberFormatter.string(from: NSNumber(value: throwsPerRound))!)
 		print("Maximum throws in a round:", numberFormatter.string(from: NSNumber(value: mostThrows))!)
 
 		if draws > 0 {
-			let drawPercentage: Float = Float(draws) / Float(rounds)
 			print()
 			print(numberFormatter.string(from: NSNumber(value: draws))!, "rounds out of",
 				  numberFormatter.string(from: NSNumber(value: rounds))!,
-				  "(" + percentFormatter.string(from: NSNumber(value: drawPercentage))! + ")",
+				  "(" + percentFormatter.string(from: NSNumber(value: getDrawRatio(rounds)))! + ")",
 				  "ended in a draw after",
 				  numberFormatter.string(from: NSNumber(value: mostThrows))!, "throws.")
 		}
 	}
 
+
+	func getDrawRatio(_ rounds: Int) -> Float {
+		return Float(draws) / Float(rounds)
+	}
+
 }
+
 
 extension String {
 	func leftPadding(toLength: Int, withPad character: Character) -> String {
